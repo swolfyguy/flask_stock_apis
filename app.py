70f1f9304022d9216d1734a6e3ddc40d1b64e6e6
sandeep <kokaresandeep94@gmail.com>
2021-07-17 23:40:57 +0530

    database changes testing

--- app.py
+++ app.py
@@ -8,14 +8,18 @@ from extensions import register_extensions
 from models.option_chain import OptionChain
 from views import register_base_routes
 from views import register_json_routes
-
+import logging
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 app = Flask(__name__, instance_relative_config=False)
-register_extensions(app)
-register_json_routes(app)
-register_base_routes(app)
+try:
+    register_extensions(app)
+    register_json_routes(app)
+    register_base_routes(app)
+except Exception as e:
+    logging.info(f"error while running :{e}")
+
 # return app
 
 
