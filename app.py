93fe07c2e37eb60af880ca5d0e7b2eb2b42bcbf6
swolfyguy <58931830+swolfyguy@users.noreply.github.com>
2021-07-20 11:00:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from extensions import register_extensions
 from models.option_chain import OptionChain
 from views import register_base_routes
 from views import register_json_routes
+from flask_cors import CORS
 
 
 def _create_app():
     """Base Flask app factory used by all apps."""
     # Create the Flask application
     app = Flask(__name__, instance_relative_config=False)
+    CORS(app)
     register_extensions(app)
     return app
 
