936ee5ec50f1e8989c6f2a7fe95f4b3b91ff2ef6
MaheshKok <maheshkokare100@gmail.com>
2021-09-23 19:53:33 +0530

    provided support for fetching expiry at runtime

--- apis/nfo.py
+++ apis/nfo.py
@@ -2,6 +2,8 @@
 import json
 import logging
 from datetime import datetime
+from datetime import date
+from nsepy.derivatives import get_expiry_date
 
 from flask import request
 from flask_rest_jsonapi import ResourceDetail
@@ -105,7 +107,21 @@ def get_constructed_data(options_data_lst=None):
 
 
 def get_options_data_list(symbol="BANKNIFTY"):
-    res = fetch_data(symbol)
+    todays_date = date.today()
+    current_month_expiry_list = sorted(
+        get_expiry_date(year=todays_date.year, month=todays_date.month, index=symbol)
+    )
+    options_expiry_dt = None
+    for dt in current_month_expiry_list:
+        if dt > todays_date:
+            options_expiry_dt = dt
+            break
+
+    formatted_expiry = ""
+    if options_expiry_dt:
+        formatted_expiry = f"{options_expiry_dt.day}{options_expiry_dt.strftime('%b').upper()}{options_expiry_dt.year}"
+
+    res = fetch_data(symbol, expiry=formatted_expiry)
     return json.loads(res.json()["OptionChainInfo"])
 
 
