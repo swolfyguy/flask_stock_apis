820244fc5aff88425935ac4b246c2c2dceefbf11
MaheshKok <maheshkokare100@gmail.com>
2022-01-08 20:37:36 +0530

    testing scheduler

--- scheduler/daily_profits.py
+++ scheduler/daily_profits.py
@@ -8,27 +8,33 @@ from extensions import db
 from main import app
 from models.daily_profits import DailyProfits
 
+#
+# @schedule.repeat(schedule.every().day.at("10:30"))
+# def update_daily_profits():
+#     print("running jobs to update profit every weekday")
+#     todays_date = datetime.today().date()
+#     if todays_date.weekday() < 5:
+#         with app.app_context():
+#             response = get_computed_profit()
+#             for strategy_profit in response["data"]:
+#                 df = DailyProfits(
+#                     strategy_id=strategy_profit["id"],
+#                     profit=strategy_profit["total"]["profit"],
+#                     date=todays_date,
+#                 )
+#                 db.session.add(df)
+#             try:
+#                 db.session.commit()
+#             except Exception as e:
+#                 print(f"Error occurred while updating {todays_date} profit: {e}")
+#                 db.session.rollback()
 
 
-@schedule.repeat(schedule.every().day.at("10:30"))
-def update_daily_profits():
-    print("running jobs to update profit every weekday")
-    todays_date = datetime.today().date()
-    if todays_date.weekday() < 5:
-        with app.app_context():
-            response = get_computed_profit()
-            for strategy_profit in response["data"]:
-                df = DailyProfits(
-                    strategy_id=strategy_profit["id"],
-                    profit=strategy_profit["total"]["profit"],
-                    date=todays_date,
-                )
-                db.session.add(df)
-            try:
-                db.session.commit()
-            except Exception as e:
-                print(f"Error occurred while updating {todays_date} profit: {e}")
-                db.session.rollback()
+@schedule.repeat(schedule.every(3).seconds)
+def log_time():
+    print("running jobs to log time")
+    current_time = datetime.now()
+    print(f"running jobs to log time: {current_time}")
 
 
 while True:
