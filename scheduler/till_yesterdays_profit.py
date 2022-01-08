ba99e8498e64e71bd42b3e2bb445cde79f8a49cc
MaheshKok <maheshkokare100@gmail.com>
2022-01-08 21:56:37 +0530

    commented if condition

--- scheduler/till_yesterdays_profit.py
+++ scheduler/till_yesterdays_profit.py
@@ -38,21 +38,21 @@ from models.till_yesterdays_profit import TillYesterdaysProfit
 def log_time():
     print("running jobs to update profit every weekday")
     todays_date = datetime.today().date()
-    if todays_date.weekday() < 5:
-        with app.app_context():
-            response = get_computed_profit()
-            for strategy_profit in response["data"]:
-                df = TillYesterdaysProfit(
-                    strategy_id=strategy_profit["id"],
-                    profit=strategy_profit["total"]["profit"],
-                    date=todays_date,
-                )
-                db.session.add(df)
-            try:
-                db.session.commit()
-            except Exception as e:
-                print(f"Error occurred while updating {todays_date} profit: {e}")
-                db.session.rollback()
+    # if todays_date.weekday() < 5:
+    with app.app_context():
+        response = get_computed_profit()
+        for strategy_profit in response["data"]:
+            df = TillYesterdaysProfit(
+                strategy_id=strategy_profit["id"],
+                profit=strategy_profit["total"]["profit"],
+                date=todays_date,
+            )
+            db.session.add(df)
+        try:
+            db.session.commit()
+        except Exception as e:
+            print(f"Error occurred while updating {todays_date} profit: {e}")
+            db.session.rollback()
 
 
 while True:
